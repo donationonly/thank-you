0065dc1f5f646a6621687b47f9dc9b5a0cc48f3d
donationonly <98056494+donationonly@users.noreply.github.com>
2022-07-10 05:53:45 -0400

    10/07/2022

--- main.py
+++ main.py
@@ -21,15 +21,19 @@ init(autoreset=True)
 def logTime():
     now_utc = datetime.now(timezone('UTC'))
     now_pacific = now_utc.astimezone(timezone("Asia/Jakarta"))
-    return now_pacific.strftime("%H:%M")
+    return now_pacific.strftime("%H:%M:%S")
 
 def banner(str):
     os.system("cls||clear")
     __banner__ = pyfiglet.figlet_format(str, font="slant", justify="center")
     print(red + __banner__)
-    print(f"{white}Tools by {purple}!@#$")
-    print(f"{white}Update {red}06/07/2022")
+    print(f"{purple}Donation Scripts")
+    print(f"{white}Update {red}10/07/2022")
     print(f"{white}Trophy & Crown Duplicator For Stumbe Guys\n")
+
+    print(f"{white}Format : [Time] Username | Trophy | Crown ")
+    print(f"{yellow}Setelah memasukan delay tidak muncul apa apa, berarti auth token sudah expired!")
+    print(f"{white}")
 
 
 def start():
@@ -39,8 +43,8 @@ def start():
 1. Round 1 ( 10 Trophy)
 2. Round 2 ( 20 Trophy )
 3. Round 3 ( Crown )""")
-    round_input = input(f"Enter round (1, 2, 3) : ")
-    delay_input = input(f"Enter Delay (ex: 1 = 1second) : ")
+    round_input = input(f"Select you round : ")
+    delay_input = input(f"Enter Delay ( in second ) : ")
 
     while True:
         try:
@@ -54,7 +58,7 @@ def start():
             elif "SERVER_ERROR" in str(req_game):
                 continue
             elif "User" in str(req_game):
-                print(f"{red}[{white}{logTime()}{red}] {white}Nickname: {magenta}{req_game['User']['Username']} {white}| Country: {blue}{req_game['User']['Country']} {white}| Trophy: {green}{req_game['User']['SkillRating']} {white}| Crown: {yellow}{req_game['User']['Crowns']}")
+                print(f"{white}[{white}{logTime()}{white}] {white}{req_game['User']['Username']} {white}| {req_game['User']['Country']} {white}| {req_game['User']['SkillRating']} {white}| {req_game['User']['Crowns']}")
         except:
             continue
 
